01ae52d2b7bfe950419d0d57d846dec9c53b5be6
Vichet MOENG <v.moeng@testingstudio.io>
2021-05-08 12:46:35 +0700

    update user and admin logout functionality

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -43,8 +43,13 @@
                                             <ul class="submenu" >
                                                 <li class="menu-item" >
                                                     <a title="Dashboard" href="{{ route('admin.dashboard') }}">Dashboard</a>
-                                                    <a title="Logout" href="#">Logout</a>
                                                 </li>
+                                                <li class="menu-item">
+                                                    <a title="Logout" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('admin-logout-form').submit();">Logout</a>
+                                                </li>
+                                                <form id="admin-logout-form" method="POST" action="{{ route('logout') }}">
+                                                    @csrf
+                                                </form>
                                             </ul>
                                         </li>
                                     @else
@@ -53,8 +58,13 @@
                                             <ul class="submenu" >
                                                 <li class="menu-item" >
                                                     <a title="Dashboard" href="{{ route('user.dashboard') }}">Dashboard</a>
-                                                    <a title="logout" href="#">Logout</a>
                                                 </li>
+                                                <li class="menu-item">
+                                                    <a title="Logout" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('user-logout-form').submit();">Logout</a>
+                                                </li>
+                                                <form id="user-logout-form" method="POST" action="{{ route('logout') }}">
+                                                    @csrf
+                                                </form>
                                             </ul>
                                         </li>
                                     @endif
