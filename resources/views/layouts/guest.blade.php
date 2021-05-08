f267f6d90bec7a757178829c80fa3a0a95382a7c
Vichet MOENG <v.moeng@testingstudio.io>
2021-05-08 13:48:41 +0700

    update login, register and forget password front end

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,24 +1,272 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/ourfavicon.ico')}}">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,700,700italic,900,900italic&amp;subset=latin,latin-ext" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open%20Sans:300,400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/animate.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/chosen.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/color-01.css')}}">
+    @livewireStyles
+</head>
+<body class="home-page home-01 ">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<!-- mobile menu -->
+<div class="mercado-clone-wrap">
+    <div class="mercado-panels-actions-wrap">
+        <a class="mercado-close-btn mercado-close-panels" href="#">x</a>
+    </div>
+    <div class="mercado-panels"></div>
+</div>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+<!--header-->
+<header id="header" class="header header-style-1">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="topbar-menu-area">
+                <div class="container">
+                    <div class="topbar-menu right-menu">
+                        <ul>
+                            @if(Route::has('login'))
+                                @auth
+                                    @if(Auth::user()->utype === 'ADM')
+                                        <li class="menu-item menu-item-has-children parent" >
+                                            <a title="My account" href="#">My Account ({{Auth::user()->name}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                            <ul class="submenu" >
+                                                <li class="menu-item" >
+                                                    <a title="Dashboard" href="{{ route('admin.dashboard') }}">Dashboard</a>
+                                                </li>
+                                                <li class="menu-item">
+                                                    <a title="Logout" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('admin-logout-form').submit();">Logout</a>
+                                                </li>
+                                                <form id="admin-logout-form" method="POST" action="{{ route('logout') }}">
+                                                    @csrf
+                                                </form>
+                                            </ul>
+                                        </li>
+                                    @else
+                                        <li class="menu-item menu-item-has-children parent" >
+                                            <a title="My account" href="#">My Account ({{Auth::user()->name}})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                            <ul class="submenu" >
+                                                <li class="menu-item" >
+                                                    <a title="Dashboard" href="{{ route('user.dashboard') }}">Dashboard</a>
+                                                </li>
+                                                <li class="menu-item">
+                                                    <a title="Logout" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('user-logout-form').submit();">Logout</a>
+                                                </li>
+                                                <form id="user-logout-form" method="POST" action="{{ route('logout') }}">
+                                                    @csrf
+                                                </form>
+                                            </ul>
+                                        </li>
+                                    @endif
+                                @else
+                                    <li class="menu-item" ><a title="Register or Login" href="{{ route('login') }}">Login</a></li>
+                                    <li class="menu-item" ><a title="Register or Login" href="{{ route('register') }}">Register</a></li>
+                                @endif
+                            @endif
+                        </ul>
+                    </div>
+                </div>
+            </div>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+            <div class="container">
+                <div class="mid-section main-info-area">
 
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
+                    <div class="wrap-logo-top left-section">
+                        <a href="/" class="link-to-home"><img src="{{ asset('assets/images/logo-top-1.png') }}" alt="mercado"></a>
+                    </div>
+
+                    <div class="wrap-search center-section">
+                        <div class="wrap-search-form">
+                            <form action="#" id="form-search-top" name="form-search-top">
+                                <input type="text" name="search" value="" placeholder="Search here...">
+                                <button form="form-search-top" type="button"><i class="fa fa-search" aria-hidden="true"></i></button>
+                                <div class="wrap-list-cate">
+                                    <input type="hidden" name="product-cate" value="0" id="product-cate">
+                                    <a href="#" class="link-control">All Category</a>
+                                    <ul class="list-cate">
+                                        <li class="level-0">All Category</li>
+                                        <li class="level-1">-Electronics</li>
+                                        <li class="level-2">Batteries & Chargens</li>
+                                        <li class="level-2">Headphone & Headsets</li>
+                                        <li class="level-2">Mp3 Player & Acessories</li>
+                                        <li class="level-1">-Smartphone & Table</li>
+                                        <li class="level-2">Batteries & Chargens</li>
+                                        <li class="level-2">Mp3 Player & Headphones</li>
+                                        <li class="level-2">Table & Accessories</li>
+                                        <li class="level-1">-Electronics</li>
+                                        <li class="level-2">Batteries & Chargens</li>
+                                        <li class="level-2">Headphone & Headsets</li>
+                                        <li class="level-2">Mp3 Player & Acessories</li>
+                                        <li class="level-1">-Smartphone & Table</li>
+                                        <li class="level-2">Batteries & Chargens</li>
+                                        <li class="level-2">Mp3 Player & Headphones</li>
+                                        <li class="level-2">Table & Accessories</li>
+                                    </ul>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div class="wrap-icon right-section">
+                        <div class="wrap-icon-section minicart pull-right">
+                            <a href="#" class="link-direction">
+                                <i class="fa fa-shopping-basket" aria-hidden="true"></i>
+                                <div class="left-info">
+                                    <span class="index">4 items</span>
+                                    <span class="title">CART</span>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="nav-section header-sticky">
+                <div class="primary-nav-section">
+                    <div class="container">
+                        <ul class="nav primary clone-main-menu" id="mercado_main" data-menuname="Main menu" >
+                            <li class="menu-item home-icon">
+                                <a href="/" class="link-term mercado-item-title"><i class="fa fa-home" aria-hidden="true"></i></a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="about-us.html" class="link-term mercado-item-title">About Us</a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="/shop" class="link-term mercado-item-title">Shop</a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="/cart" class="link-term mercado-item-title">Cart</a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="/checkout" class="link-term mercado-item-title">Checkout</a>
+                            </li>
+                            <li class="menu-item">
+                                <a href="contact-us.html" class="link-term mercado-item-title">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-    </body>
+    </div>
+</header>
+
+{{$slot}}
+
+<footer id="footer">
+    <div class="wrap-footer-content footer-style-1">
+        <hr>
+        <!--End function info-->
+
+        <div class="main-footer-content">
+
+            <div class="container">
+
+                <div class="row">
+
+                    <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+                        <div class="wrap-footer-item">
+                            <h3 class="item-header">Contact Details</h3>
+                            <div class="item-content">
+                                <div class="wrap-contact-detail">
+                                    <ul>
+                                        <li>
+                                            <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                            <p class="contact-txt">45 Grand Central Terminal New York,NY 1017 United State USA</p>
+                                        </li>
+                                        <li>
+                                            <i class="fa fa-phone" aria-hidden="true"></i>
+                                            <p class="contact-txt">(+123) 456 789 - (+123) 666 888</p>
+                                        </li>
+                                        <li>
+                                            <i class="fa fa-envelope" aria-hidden="true"></i>
+                                            <p class="contact-txt">Contact@yourcompany.com</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 col-sm-6 col-md-6 col-xs-12 box-twin-content ">
+                        <div class="row">
+                            <div class="wrap-footer-item twin-item">
+                                <h3 class="item-header">My Account</h3>
+                                <div class="item-content">
+                                    <div class="wrap-vertical-nav">
+                                        <ul>
+                                            <li class="menu-item"><a href="#" class="link-term">My Account</a></li>
+                                            <li class="menu-item"><a href="#" class="link-term">Brands</a></li>
+                                            <li class="menu-item"><a href="#" class="link-term">Gift Certificates</a></li>
+                                            <li class="menu-item"><a href="#" class="link-term">Affiliates</a></li>
+                                            <li class="menu-item"><a href="#" class="link-term">Wish list</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="wrap-footer-item twin-item">
+                                <h3 class="item-header">Infomation</h3>
+                                <div class="item-content">
+                                    <div class="wrap-vertical-nav">
+                                        <ul>
+                                            <li class="menu-item"><a href="#" class="link-term">Contact Us</a></li>
+                                            <li class="menu-item"><a href="#" class="link-term">Returns</a></li>
+                                            <li class="menu-item"><a href="#" class="link-term">Site Map</a></li>
+                                            <li class="menu-item"><a href="#" class="link-term">Specials</a></li>
+                                            <li class="menu-item"><a href="#" class="link-term">Order History</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+        </div>
+
+        <div class="coppy-right-box">
+            <div class="container">
+                <div class="coppy-right-item item-left">
+                    <p class="coppy-right-text">Copyright © {{ date('Y') }} VCVS Book Store Group. All rights reserved</p>
+                </div>
+                <div class="coppy-right-item item-right">
+                    <div class="wrap-nav horizontal-nav">
+                        <ul>
+                            <li class="menu-item"><a href="about-us.html" class="link-term">About us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="{{asset('assets/js/jquery-1.12.4.minb8ff.js?ver=1.12.4')}}"></script>
+<script src="{{asset('assets/js/jquery-ui-1.12.4.minb8ff.js?ver=1.12.4')}}"></script>
+<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/js/jquery.flexslider.js')}}"></script>
+<script src="{{asset('assets/js/chosen.jquery.min.js')}}"></script>
+<script src="{{asset('assets/js/owl.carousel.min.js')}}"></script>
+<script src="{{asset('assets/js/jquery.countdown.min.js')}}"></script>
+<script src="{{asset('assets/js/jquery.sticky.js')}}"></script>
+<script src="{{asset('assets/js/functions.js')}}"></script>
+@livewireScripts
+</body>
 </html>
