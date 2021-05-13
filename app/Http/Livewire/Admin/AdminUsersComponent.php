50b5424bd7a41cc0b2d25e7c19ffa11896db9b97
Vichet MOENG <v.moeng@testingstudio.io>
2021-05-13 20:43:39 +0700

    update admin to able change his information

--- app/Http/Livewire/Admin/AdminUsersComponent.php
+++ app/Http/Livewire/Admin/AdminUsersComponent.php
@@ -4,10 +4,62 @@ namespace App\Http\Livewire\Admin;
 
 use App\Models\User;
 use Artesaos\SEOTools\Facades\SEOTools;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class AdminUsersComponent extends Component
 {
+    public $user_id;
+    public $name;
+    public $email;
+    public $password;
+    public $confirmpassword;
+
+    public function updated($fields)
+    {
+        $this->validateOnly($fields, [
+            'name' => 'required',
+            'email' => 'unique:users|required|email',
+            'password' => 'required|min:8',
+            'confirmpassword' => 'required_with:password|same:password|min:8',
+        ]);
+    }
+
+    public function updateInfo()
+    {
+        if ($this->email != Auth::user()->email){
+            $this->validate([
+                'name' => 'required',
+                'email' => 'unique:users|required|email',
+            ]);
+        } else {
+            $this->validate([
+                'name' => 'required',
+                'email' => 'required|email',
+            ]);
+        }
+
+        $user = User::find(Auth::id());
+        $user->name = $this->name;
+        $user->email = $this->email;
+        $user->save();
+        session()->flash('updateinfo', 'Admin information updated!');
+
+    }
+
+    public function changePassword()
+    {
+        $this->validate([
+            'password' => 'required|min:8',
+            'confirmpassword' => 'required_with:password|same:password|min:8',
+        ]);
+
+        $user = User::find(Auth::id());
+        $user->password = Hash::make($this->password);
+        $user->save();
+        session()->flash('passwordchanged', 'Admin password has been change!');
+    }
 
     public function deleteUser($id)
     {
@@ -16,6 +68,8 @@ class AdminUsersComponent extends Component
     }
     public function mount()
     {
+        $this->name = Auth::user()->name;
+        $this->email = Auth::user()->email;
         SEOTools::setTitle('Admin | VCVS Book Store Group');
     }
     public function render()
--- resources/views/livewire/admin/admin-users-component.blade.php
+++ resources/views/livewire/admin/admin-users-component.blade.php
@@ -38,7 +38,130 @@
         </div>
         <!--/col-->
         <div class="col-md-9 col-lg-10 main">
+            <div class="row mb-3">
+                <div class="col-12 col-xl-12 col-lg-12">
 
+                    <div>
+                        <style>
+                            nav svg{
+                                height: 20px;
+                            }
+                            nav .hidden{
+                                display: block !important;
+                            }
+                        </style>
+                        <div class="container" style="padding: 30px 0;">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="panel panel-success">
+                                        <div class="panel-heading">
+                                            <div class="row">
+                                                <div class="col">
+                                                    Admin information
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="panel-body">
+                                            @if(Session::has('message'))
+                                                <div class="alert alert-success" role="alert">{{ Session::get('message') }}</div>
+                                            @endif
+                                            <div class="row">
+                                                <div class="col-lg-6">
+                                                    <div class="panel panel-success">
+                                                        <div class="panel-heading">
+                                                            Information
+                                                        </div>
+                                                        <div class="panel-body container-fluid">
+                                                            <p><strong>Name: </strong>{{ $name }}</p>
+                                                            <p><strong>Email: </strong>{{ $email }}</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="panel panel-success">
+                                                        <div class="panel-heading">
+                                                            Update info
+                                                        </div>
+                                                        <div class="panel-body container-fluid">
+                                                            @if(Session::has('updateinfo'))
+                                                                <div class="alert alert-success" role="alert">{{ Session::get('updateinfo') }}</div>
+                                                            @endif
+                                                            <form class="form-horizontal" enctype="multipart/form-data" wire:submit.prevent="updateInfo">
+                                                                <div class="form-group">
+                                                                    <label class="col-md-4 control-label">Name</label>
+                                                                    <div class="col-md-4">
+                                                                        <input type="text" placeholder="Name" class="form-control input-md" wire:model="name" />
+                                                                        @error('name') <span class="text-danger">{{ $message }}</span> @enderror
+                                                                    </div>
+                                                                </div>
+
+                                                                <div class="form-group">
+                                                                    <label class="col-md-4 control-label">Email</label>
+                                                                    <div class="col-md-4">
+                                                                        <input type="email" placeholder="Email" class="form-control input-md" wire:model="email" />
+                                                                        @error('email') <span class="text-danger">{{ $message }}</span> @enderror
+                                                                    </div>
+                                                                </div>
+
+                                                                <div class="form-group">
+                                                                    <label class="col-md-4 control-label"></label>
+                                                                    <div class="col-md-4">
+                                                                        <button type="submit" class="btn btn-primary">Update</button>
+                                                                    </div>
+                                                                </div>
+
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-lg-6">
+
+                                                    <div class="panel panel-success">
+                                                        <div class="panel-heading">
+                                                            Change Password
+                                                        </div>
+                                                        <div class="panel-body">
+                                                            @if(Session::has('passwordchanged'))
+                                                                <div class="alert alert-success" role="alert">{{ Session::get('passwordchanged') }}</div>
+                                                            @endif
+                                                            <form class="form-horizontal" enctype="multipart/form-data" wire:submit.prevent="changePassword">
+                                                                <div class="form-group">
+                                                                    <label class="col-md-4 control-label">New Password</label>
+                                                                    <div class="col-md-4">
+                                                                        <input type="password" placeholder="New Password" class="form-control input-md" wire:model="password" />
+                                                                        @error('password') <span class="text-danger">{{ $message }}</span> @enderror
+                                                                    </div>
+                                                                </div>
+
+                                                                <div class="form-group">
+                                                                    <label class="col-md-4 control-label">Confirm Password</label>
+                                                                    <div class="col-md-4">
+                                                                        <input type="password" placeholder="Confirm Password" class="form-control input-md" wire:model="confirmpassword" />
+                                                                        @error('confirmpassword') <span class="text-danger">{{ $message }}</span> @enderror
+                                                                    </div>
+                                                                </div>
+
+                                                                <div class="form-group">
+                                                                    <label class="col-md-4 control-label"></label>
+                                                                    <div class="col-md-4">
+                                                                        <button type="submit" class="btn btn-primary">Change Password</button>
+                                                                    </div>
+                                                                </div>
+
+                                                            </form>
+                                                        </div>
+                                                    </div>
+
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
             <div class="row mb-3">
                 <div class="col-12 col-xl-12 col-lg-12">
 
